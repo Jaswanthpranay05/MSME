6888438a197f56cc8fdeb6fde48643cad8dbbb09
PALURU JASWANTH <155042203+Jaswanthpranay05@users.noreply.github.com>
2026-03-04 20:00:35 +0530

    Update Results.tsx

--- msme_frontend/src/pages/Results.tsx
+++ msme_frontend/src/pages/Results.tsx
@@ -62,7 +62,7 @@ const Results = () => {
 
     const fetchPrediction = async () => {
       try {
-        const res = await fetch("http://localhost:8000/predict", {
+        const res = await fetch("https://msme-g3ht.onrender.com/predict", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
